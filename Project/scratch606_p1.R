3e22f23331f845468d65edee94dba3ca203d12ed
David <dsimbandumwe@gmail.com>
2021-09-25 18:35:06 -0400

    new project files

---
+++ Project/scratch606_p1.R
@@ -0,0 +1,20 @@
+
+library(tidyverse)
+library(readtext)
+
+
+
+#https://covidtracking.com/race
+
+rt_df <- read_csv( file = "./Project/CRDT_Data_CRDT.csv")
+rt_df
+
+
+
+#inf_df <- read_csv( file = "./Project/cases_by_infection.csv")
+#inf_df
+
+
+
+
+
